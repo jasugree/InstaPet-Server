d90879fde2f435208d2f2a2bfc4e7240f2097825
Courtney Downs <courtneysdowns@gmail.com>
2021-09-05 11:41:33 -0400

    First push. Creating Courtney Branch. 09/05/2021.

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const Sequelize = require("sequelize");
 
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize("InstaPet", "postgres", "eleven11F!fty50", {
-	host: "localhost",
-	dialect: "postgres",
+const sequelize = new Sequelize("InstaPet", "postgres", "password", {
+  host: "localhost",
+  dialect: "postgres",
 });
 
 sequelize
-	.authenticate()
-	.then(() => {
-		console.log("Connection has been established successfully.");
-	})
-	.catch((err) => {
-		console.error("Unable to connect to the database:", err);
-	});
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
