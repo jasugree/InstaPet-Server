5baabe40c380c0ad40f52ac9469da99122c25ca0
Jake Greene <jsgreene208@gmail.com.com>
2021-09-12 15:24:50 -0400

    added styling and mapping to home page

--- models/post.js
+++ models/post.js
@@ -1,27 +1,27 @@
 //POST MODEL FILE -- This is for you Mitch
 
 module.exports = function (sequelize, DataTypes) {
-  const Post = sequelize.define("post", {
-    image: {
-      type: DataTypes.STRING(500),
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    owner: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  });
-  return Post;
+	const Post = sequelize.define("post", {
+		image: {
+			type: DataTypes.STRING(500),
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		likes: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		category: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		owner: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	});
+	return Post;
 };
